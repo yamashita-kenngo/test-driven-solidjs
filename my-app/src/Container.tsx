5ad37dc6ea46843bdcfdb1277868e4a2eeaf1d81
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-03-19 11:59:57 +0900

    feat: add empty markdown component and routing

--- my-app/src/Container.tsx
+++ my-app/src/Container.tsx
@@ -1,9 +1,17 @@
 import type { Component } from 'solid-js';
+import { Routes, Route, A } from '@solidjs/router';
+import Markdown  from './Markdown';
 
 const Container: Component = () => {
   return (
     <div>
       <h1>Hello Solidjs</h1>
+      <nav>
+        <A href="/markdown">Markdown</A>
+      </nav>
+      <Routes>
+        <Route path="/markdown" component={Markdown} />
+      </Routes>
     </div>
   );
 };
---
+++ my-app/src/Markdown.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const Markdown: Component = () => {
+  return (
+    <div>
+      <h1>Hello Markdown</h1>
+    </div>
+  );
+};
+
+export default Markdown;
