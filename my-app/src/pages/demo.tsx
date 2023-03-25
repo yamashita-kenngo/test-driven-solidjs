fbf6dbef3de61a680f1a60f047cd76d4d44f2295
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2023-03-25 21:26:47 +0900

    chore: add input

--- my-app/src/pages/demo.tsx
+++ my-app/src/pages/demo.tsx
@@ -1,14 +1,20 @@
 import type { Component } from "solid-js";
 import Button  from "./../ui/button";
-
 const Demo: Component = () => {
+  let input!: HTMLInputElement;
+  let calendarEl!: HTMLDivElement;
+  const onSubmit = () => {
+    if(!input.value.trim()) return;
+    console.log(input.value)
+  };
   return (
     <div class="bg-green-200 h-24 min-h-full">
       <h1>Demo</h1>
+      <input type="text" ref={input}/>
       <Button
         color="accent"
         size="small"
-        onClick={() => console.log('Clicked')}
+        onClick={() => {console.log('Clicked'); onSubmit(); init();}}
       >
         Click Me
       </Button>
